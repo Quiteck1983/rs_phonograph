fd3ccc270c1961bc2f10b36217354d74c8a04973
riversafe33 <riversafe33@outlook.com>
2025-06-10 09:59:36 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,20 +2,8 @@ local VORPcore = exports.vorp_core:GetCore()
 local VorpInv = exports.vorp_inventory:vorp_inventoryApi()
 local currentlyPlaying = {}
 
-RegisterNetEvent('rs_phonograph:server:playMusic')
-AddEventHandler('rs_phonograph:server:playMusic', function(id, coords, url, volume)
-    if currentlyPlaying[id] then
-        return
-    end
-
-    currentlyPlaying[id] = {
-        url = url,
-        volume = volume,
-        coords = coords,
-    }
-
+RegisterNetEvent('rs_phonograph:server:playMusic', function(id, coords, url, volume)
     TriggerClientEvent('rs_phonograph:client:playMusic', -1, id, coords, url, volume)
-
 end)
 
 RegisterNetEvent('rs_phonograph:server:saveOwner')
