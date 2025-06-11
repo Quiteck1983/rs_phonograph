9c47edf7c3a161a0e6105a0a3bf04f0f818271e4
riversafe33 <riversafe33@outlook.com>
2025-06-11 11:38:03 +0200

    the Net ID has been removed

--- client.lua
+++ client.lua
@@ -1,7 +1,4 @@
-local soundId = 'rs_phonograph'
 local volume = 0.3
-local phonographEntities = {} 
-
 local Menu = exports.vorp_menu:GetMenuData()
 
 local function OpenSongListMenu(labelName, songs, uniqueId)
@@ -46,7 +43,7 @@ local function OpenSongListMenu(labelName, songs, uniqueId)
     end)
 end
 
-local function OpenPhonographMenu(entity, networkEntityId, uniqueId)
+local function OpenPhonographMenu(entity, uniqueId)
     Menu.CloseAll()
 
     local function BuildElements()
@@ -122,7 +119,7 @@ local function OpenPhonographMenu(entity, networkEntityId, uniqueId)
             local result = exports.vorp_inputs:advancedInput(myInput)
 
             if result and result:sub(1, 4) == "http" then
-                TriggerServerEvent('rs_phonograph:server:playMusic', id, GetEntityCoords(entity), result, volume)
+                TriggerServerEvent('rs_phonograph:server:playMusic', uniqueId, GetEntityCoords(entity), result, volume)
                 TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.PlayMessage, "generic_textures", "tick", 1500, "GREEN")
             else
                 TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.InvalidUrlMessage, "menu_textures", "cross", 500, "COLOR_RED")
@@ -130,18 +127,18 @@ local function OpenPhonographMenu(entity, networkEntityId, uniqueId)
 
         elseif data.current.value == "choose_song" then
             menu.close()
-            OpenSongListMenu(Config.Menu.SongList, Config.SongList, id)
+            OpenSongListMenu(Config.Menu.SongList, Config.SongList, uniqueId)
 
         elseif data.current.value == "stop" then
-            TriggerServerEvent('rs_phonograph:server:stopMusic', id)
+            TriggerServerEvent('rs_phonograph:server:stopMusic', uniqueId)
             TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.StopMessage, "menu_textures", "stop", 500, "COLOR_RED")
 
         elseif data.current.value == "volume_up" then
             if volume < 1.0 then
                 volume = volume + 0.05
                 if volume > 1.0 then volume = 1.0 end
-                TriggerServerEvent('rs_phonograph:server:setVolume', id, volume)
-                TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.VolumeUpMessage:format(math.floor(volume * 100)), "generic_textures", "tick", 500, "GREEN")
+                TriggerServerEvent('rs_phonograph:server:setVolume', uniqueId, volume)
+                TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.VolumeUpMessage:format(math.floor(volume * 100)), "generic_textures", "tick", 100, "GREEN")
             else
                 TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.MaxVolumeMessage, "menu_textures", "cross", 500, "COLOR_RED")
             end
@@ -150,8 +147,8 @@ local function OpenPhonographMenu(entity, networkEntityId, uniqueId)
             if volume > 0.0 then
                 volume = volume - 0.05
                 if volume < 0.0 then volume = 0.0 end
-                TriggerServerEvent('rs_phonograph:server:setVolume', id, volume)
-                TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.VolumeDownMessage:format(math.floor(volume * 100)), "generic_textures", "tick", 500, "GREEN")
+                TriggerServerEvent('rs_phonograph:server:setVolume', uniqueId, volume)
+                TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.VolumeDownMessage:format(math.floor(volume * 100)), "generic_textures", "tick", 100, "GREEN")
             else
                 TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.MinVolumeMessage, "menu_textures", "cross", 500, "COLOR_RED")
             end
@@ -162,7 +159,6 @@ local function OpenPhonographMenu(entity, networkEntityId, uniqueId)
     end)
 end
 
-
 local promptGroup = UipromptGroup:new(Config.Promp.Controls)
 
 local playMusicPrompt = Uiprompt:new(`INPUT_DYNAMIC_SCENARIO`, Config.Promp.Play, promptGroup)
@@ -172,25 +168,37 @@ local pickUpPrompt = Uiprompt:new(`INPUT_RELOAD`, Config.Promp.Collect, promptGr
 pickUpPrompt:setHoldMode(true)
 
 local closestEntity = nil
+local closestId = nil
+local pendingPhonographObject = nil
+phonographEntities = {}
 
 local function updatePrompts()
     local playerPed = PlayerPedId()
     local playerCoords = GetEntityCoords(playerPed)
-    closestEntity = nil
-
+    closestEntity, closestId = nil, nil
     local found = false
 
-    for netId, uniqueId in pairs(phonographEntities or {}) do
-        if NetworkDoesNetworkIdExist(netId) and NetworkDoesEntityExistWithNetworkId(netId) then
-            local entity = NetworkGetEntityFromNetworkId(netId)
+    for uniqueId, entity in pairs(phonographEntities or {}) do
+        if DoesEntityExist(entity) then
             local entityCoords = GetEntityCoords(entity)
             local distance = #(playerCoords - entityCoords)
+            print(string.format("[PROMPT] Checking entity %s at distance %.2f", uniqueId, distance)) -- Debug
 
-            if GetEntityModel(entity) == GetHashKey('p_phonograph01x') and distance <= 1.5 then
+            if distance <= 1.5 then
                 closestEntity = entity
+                closestId = uniqueId
                 found = true
                 break
             end
+        end
+    end
+
+    if not found and pendingPhonographObject and DoesEntityExist(pendingPhonographObject) then
+        local distance = #(playerCoords - GetEntityCoords(pendingPhonographObject))
+        if distance <= 1.5 then
+            closestEntity = pendingPhonographObject
+            closestId = nil
+            found = true
         end
     end
 
@@ -201,47 +209,42 @@ local function updatePrompts()
     pickUpPrompt:setEnabled(found)
 end
 
-
 RegisterNetEvent('rs_phonograph:client:spawnPhonograph')
 AddEventHandler('rs_phonograph:client:spawnPhonograph', function(data)
     local propModel = GetHashKey('p_phonograph01x')
     RequestModel(propModel)
     while not HasModelLoaded(propModel) do Wait(10) end
 
-    local object = CreateObject(propModel, data.x, data.y, data.z, true, false, false)
+    local uniqueId = data.id
 
-    SetEntityRotation(object, data.rotation.x, data.rotation.y, data.rotation.z, 0, true)
-
-    phonographEntities = phonographEntities or {}
-    local netId = NetworkGetNetworkIdFromEntity(object)
-    phonographEntities[netId] = data.id
-    updatePrompts()
-end)
-
-AddEventHandler('playerSpawned', function()
-    TriggerServerEvent("rs_phonograph:server:loadPhonographs")
-end)
-
-AddEventHandler('onClientResourceStart', function(resourceName)
-    if resourceName == GetCurrentResourceName() then
-        TriggerServerEvent("rs_phonograph:server:loadPhonographs")
+    if phonographEntities[uniqueId] and DoesEntityExist(phonographEntities[uniqueId]) then
+        DeleteObject(phonographEntities[uniqueId])
     end
+
+    local object = CreateObject(propModel, data.x, data.y, data.z, true, false, false)
+    SetEntityRotation(object, data.rotation.x, data.rotation.y, data.rotation.z, 0, true)
+    SetEntityAsMissionEntity(object, true, true)
+
+
+    phonographEntities[uniqueId] = object
+
+    CreateThread(function()
+        Wait(1000)
+        updatePrompts()
+    end)
 end)
 
 promptGroup:setOnHoldModeJustCompleted(function(group, prompt)
     if closestEntity and DoesEntityExist(closestEntity) then
-        local netId = NetworkGetNetworkIdFromEntity(closestEntity)
-        local uniqueId = phonographEntities[netId]
-
         if prompt == playMusicPrompt then
-            if uniqueId then
-                OpenPhonographMenu(closestEntity, netId, uniqueId)
+            if closestId then
+                OpenPhonographMenu(closestEntity, closestId)
             else
                 TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.UnregisteredMessage, "generic_textures", "tick", 3000, "GREEN")
             end
         elseif prompt == pickUpPrompt then
-            if uniqueId then
-                TriggerServerEvent('rs_phonograph:server:pickUpByOwner', uniqueId)
+            if closestId then
+                TriggerServerEvent('rs_phonograph:server:pickUpByOwner', closestId)
             else
                 TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.UnregisteredMessage, "generic_textures", "tick", 3000, "GREEN")
             end
@@ -260,22 +263,25 @@ end)
 
 UipromptManager:startEventThread()
 
+RegisterNetEvent("vorp:SelectedCharacter")
+AddEventHandler("vorp:SelectedCharacter", function()
+    TriggerServerEvent("rs_phonograph:server:loadPhonographs")
+end)
+
+
+
 RegisterNetEvent('rs_phonograph:client:placePropPhonograph')
 AddEventHandler('rs_phonograph:client:placePropPhonograph', function()
-
     local propModel = GetHashKey('p_phonograph01x')
     RequestModel(propModel)
     while not HasModelLoaded(propModel) do Wait(10) end
 
     local playerPed = PlayerPedId()
     local px, py, pz = table.unpack(GetEntityCoords(playerPed, true))
+    local ox, oy, oz = table.unpack(GetOffsetFromEntityInWorldCoords(playerPed, 0.0, 2.5, 0.0))
 
-    local offsetDistance = 2.5
-    local ox, oy, oz = table.unpack(GetOffsetFromEntityInWorldCoords(playerPed, 0.0, offsetDistance, 0.0))
     local groundSuccess, groundZ = GetGroundZFor_3dCoord(ox, oy, pz, false)
-    if groundSuccess then
-        pz = groundZ
-    end
+    if groundSuccess then pz = groundZ end
 
     local object = CreateObject(propModel, ox, oy, pz, true, false, false)
     PlaceObjectOnGroundProperly(object)
@@ -289,84 +295,45 @@ AddEventHandler('rs_phonograph:client:placePropPhonograph', function()
     FreezeEntityPosition(object, true)
     SetEntityCollision(object, false, false)
     SetEntityAlpha(object, 150, false)
-
     SendNUIMessage({ action = "show" })
 
-    Citizen.CreateThread(function()
+    CreateThread(function()
         while isPlacing do
-            Citizen.Wait(0)
-
-            if IsControlJustPressed(0, 0x4F49CC4C) then
-                local myInput = {
-                    type = "enableinput",
-                    inputType = "input",
-                    button = Config.Menu.Confirm,
-                    placeholder = Config.Menu.MinMax,
-                    style = "block",
-                    attributes = {
-                        inputHeader = Config.Menu.Speed,
-                        type = "text",
-                        pattern = "[0-9.]+",
-                        title = Config.Menu.Change,
-                        style = "border-radius: 10px; background-color: ; border:none;"
-                    }
-                }
-
-                local result = exports.vorp_inputs:advancedInput(myInput)
-                if result and result ~= "" then
-                    local testint = tonumber(result)
-                    if testint and testint ~= 0 then
-                        moveStep = math.max(0.01, math.min(testint, 5))
-                    end
-                end
-            end
-
+            Wait(0)
             local moved = false
-            if IsControlPressed(0, 0x6319DB71) then posY = posY + moveStep; moved = true end -- UP
-            if IsControlPressed(0, 0x05CA7C52) then posY = posY - moveStep; moved = true end -- DOWN
-            if IsControlPressed(0, 0xA65EBAB4) then posX = posX - moveStep; moved = true end -- LEFT
-            if IsControlPressed(0, 0xDEB34313) then posX = posX + moveStep; moved = true end -- RIGHT
-            if IsControlPressed(0, 0xB03A913B) then posZ = posZ + moveStep; moved = true end -- 7
-            if IsControlPressed(0, 0x42385422) then posZ = posZ - moveStep; moved = true end -- 8
-            if IsControlPressed(0, 0xE6F612E4) then heading = heading + 5; moved = true end -- 1
-            if IsControlPressed(0, 0x1CE6D9EB) then heading = heading - 5; moved = true end -- 2
+
+            if IsControlPressed(0, 0x6319DB71) then posY = posY + moveStep; moved = true end
+            if IsControlPressed(0, 0x05CA7C52) then posY = posY - moveStep; moved = true end
+            if IsControlPressed(0, 0xA65EBAB4) then posX = posX - moveStep; moved = true end
+            if IsControlPressed(0, 0xDEB34313) then posX = posX + moveStep; moved = true end
+            if IsControlPressed(0, 0xB03A913B) then posZ = posZ + moveStep; moved = true end
+            if IsControlPressed(0, 0x42385422) then posZ = posZ - moveStep; moved = true end
+            if IsControlPressed(0, 0xE6F612E4) then heading = heading + 5; moved = true end
+            if IsControlPressed(0, 0x1CE6D9EB) then heading = heading - 5; moved = true end
 
             if moved then
                 SetEntityCoords(object, posX, posY, posZ, true, true, true, false)
                 SetEntityHeading(object, heading)
             end
 
-            if IsControlJustPressed(0, 0xC7B5340A) then -- ENTER
+            if IsControlJustPressed(0, 0xC7B5340A) then
                 isPlacing = false
-                
                 FreezeEntityPosition(object, true)
                 SetEntityAlpha(object, 255, false)
                 SetEntityCollision(object, true, true)
 
-                local netId = NetworkGetNetworkIdFromEntity(object)
-                local uniqueId = 'rs_phonograph' .. '-' .. netId
-                phonographEntities = phonographEntities or {}
-                phonographEntities[netId] = uniqueId
-
-                local rotation = GetEntityRotation(object, 2)
+                local rotation = GetEntityRotation(object, 0)
                 local coords = GetEntityCoords(object)
 
-                TriggerServerEvent('rs_phonograph:server:saveOwner',
-                    uniqueId,
-                    { x = coords.x, y = coords.y, z = coords.z },
-                    { x = rotation.x, y = rotation.y, z = rotation.z }
-                )
+                TriggerServerEvent('rs_phonograph:server:saveOwner', { x = coords.x, y = coords.y, z = coords.z }, { x = rotation.x, y = rotation.y, z = rotation.z })
+                
+                pendingPhonographObject = object
+
                 TriggerServerEvent("rs_phonograph:givePhonograph")
                 TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.Place, "generic_textures", "tick", 500, "GREEN")
                 SendNUIMessage({ action = "hide" })
-                updatePrompts()
-            end
 
-            if IsControlJustPressed(0, 0x760A9C6F) then -- G
-                isPlacing = false
-                DeleteObject(object)
-                TriggerEvent("vorp:NotifyLeft", Config.Notify.Phono, Config.Notify.Placed, "menu_textures", "cross", 500, "COLOR_RED")
-                SendNUIMessage({ action = "hide" })
+                updatePrompts()
             end
         end
     end)
@@ -374,25 +341,20 @@ end)
 
 RegisterNetEvent('rs_phonograph:client:removePhonograph')
 AddEventHandler('rs_phonograph:client:removePhonograph', function(uniqueId)
-    for netId, id in pairs(phonographEntities) do
-        if id == uniqueId then
-            local entity = NetworkGetEntityFromNetworkId(netId)
-            if entity and DoesEntityExist(entity) then
-                DeleteObject(entity)
-            end
-            phonographEntities[netId] = nil
-            break
-        end
+    local entity = phonographEntities[uniqueId]
+    if entity and DoesEntityExist(entity) then
+        DeleteObject(entity)
     end
+    phonographEntities[uniqueId] = nil
 end)
 
-local function getSoundName(id)
-    return tostring(id) -- nombre limpio, por ejemplo: rs_phonograph-51
+local function getSoundName(uniqueId)
+    return tostring(uniqueId)
 end
 
 RegisterNetEvent('rs_phonograph:client:playMusic')
-AddEventHandler('rs_phonograph:client:playMusic', function(id, coords, url, volume)
-    local soundName = getSoundName(id)
+AddEventHandler('rs_phonograph:client:playMusic', function(uniqueId, coords, url, volume)
+    local soundName = getSoundName(uniqueId)
 
     exports.xsound:PlayUrlPos(soundName, url, volume, coords)
     exports.xsound:Distance(soundName, 10)
@@ -410,15 +372,15 @@ AddEventHandler('rs_phonograph:client:playMusic', function(id, coords, url, volu
             if exports.xsound:soundExists(effectSoundName) then
                 exports.xsound:Destroy(effectSoundName)
             end
-            -- Notificar al servidor que terminó
-            TriggerServerEvent('rs_phonograph:server:soundEnded', id)
+
+            TriggerServerEvent('rs_phonograph:server:soundEnded', uniqueId)
         end)
     end
 end)
 
 RegisterNetEvent('rs_phonograph:client:stopMusic')
-AddEventHandler('rs_phonograph:client:stopMusic', function(id)
-    local soundName = getSoundName(id)
+AddEventHandler('rs_phonograph:client:stopMusic', function(uniqueId)
+    local soundName = getSoundName(uniqueId)
     local effectSoundName = soundName .. "_effect"
 
     if exports.xsound:soundExists(soundName) then
@@ -429,13 +391,12 @@ AddEventHandler('rs_phonograph:client:stopMusic', function(id)
         exports.xsound:Destroy(effectSoundName)
     end
 
-    -- También avisa al servidor si se detuvo manualmente
-    TriggerServerEvent('rs_phonograph:server:soundEnded', id)
+    TriggerServerEvent('rs_phonograph:server:soundEnded', uniqueId)
 end)
 
 RegisterNetEvent('rs_phonograph:client:setVolume')
-AddEventHandler('rs_phonograph:client:setVolume', function(id, newVolume)
-    local soundName = getSoundName(id)
+AddEventHandler('rs_phonograph:client:setVolume', function(uniqueId, newVolume)
+    local soundName = getSoundName(uniqueId)
 
     if exports.xsound:soundExists(soundName) then
         exports.xsound:setVolume(soundName, newVolume)
